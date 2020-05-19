3140d485f657812bb9989e3b96961f6590eb53e9
marek360 <62219914+marek360@users.noreply.github.com>
2020-05-19 16:22:08 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+
+"use strict"
+
+const button = document.getElementById("button")
+
+const quotes = [
+    `Chuck Norris ne ment pas, c'est la vérité qui se trompe.`,
+    `Chuck Norris est en couleur sur les photos Noir et Blanc..`,
+    `Un jour Chuck Norris a dit "Va voir la-bas si j'y suis"... et il y était....`,
+    `Chuck Norris a déjà compté jusqu'à l'infini. Deux fois..`,
+    `Chuck Norris est contre les radars automatiques : ça l'éblouit lorsqu'il fait du vélo..`,
+    `Quand Google ne trouve pas quelque chose, il demande à Chuck Norris.`,
+    `Chuck Norris peut encercler ses ennemis. Tout seul.`,
+    `Chuck Norris sait parler le braille..`,
+    `Chuck Norris peut monter en bas.`,
+    `Chuck Norris peut monter en bas..`
+]
+
+
+function generateQuote() {
+        return quotes[Math.floor(Math.random() * quotes.length)]
+}
+
+function generateColor() {
+    let color = []
+
+    for (let i = 0 ; i < 3 ; i++)
+        color.push(Math.floor(Math.random() * 255))
+
+    return color
+}
+
+
+function display() {
+    let text = document.getElementById("quote")
+    let color = generateColor()
+    text.textContent = generateQuote()
+    text.style.color = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+}
+
+button.addEventListener("click", display)
+
